1401883c355f1b9a17376c1510c7e31bd82cdd4a
Matthew Araujo <90223014+MatthewAraujo@users.noreply.github.com>
2023-03-29 23:46:05 -0300

    react router dom created

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { ThemeProvider } from "styled-components";
 import Blog from "./pages/Blog";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Issues from "./pages/issues";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <Blog />
+      <Router>
+        <Routes>
+          <Route path="/issues" element={<Issues />} />
+          <Route path="/" element={<Blog />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ src/pages/issues/index.tsx
@@ -0,0 +1,3 @@
+export default function Issues() {
+  return <h1>ola</h1>;
+}
